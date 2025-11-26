d26b8afda2e905a46521f34562f81cdb9d182800
Davidex01 <davidex010506@gmail.com>
2025-11-26 19:46:16 +0300

    fixed to work with back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".",           // корень проекта — текущая папка frontend
-  build: {
-    outDir: "dist",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
   },
 });
